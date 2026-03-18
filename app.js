e3a6bd19708f81e80865a78555d26d0bc03eabce
Malvika Verma <malvikaa.1708@gmail.com>
2026-03-18 13:51:13 +0530

    fix app

--- app.js
+++ app.js
@@ -10,8 +10,40 @@ const parseSlots = require("./slotParser")
 const db = require("./db")
 const createCalendarEvent = require("./googleCalendar")
 
+
 const app = express()
 app.use(express.json())
+// ✅ ADD HERE
+
+function convertToISODate(dateStr) {
+  const months = {
+    January: "01", February: "02", March: "03", April: "04",
+    May: "05", June: "06", July: "07", August: "08",
+    September: "09", October: "10", November: "11", December: "12"
+  }
+
+  const [day, month] = dateStr.split(" ")
+  const year = new Date().getFullYear()
+
+  return `${year}-${months[month]}-${day.padStart(2, "0")}`
+}
+
+function convertTo24Hour(timeStr) {
+  timeStr = timeStr.toLowerCase().trim()
+
+  let [time, modifier] = timeStr.split(" ")
+  let [hours, minutes] = time.split(":")
+
+  hours = parseInt(hours)
+  minutes = minutes ? parseInt(minutes) : 0
+
+  if (modifier === "pm" && hours !== 12) hours += 12
+  if (modifier === "am" && hours === 12) hours = 0
+
+  return `${hours.toString().padStart(2, "0")}:${minutes
+    .toString()
+    .padStart(2, "0")}:00`
+}
 
 app.get("/", (req,res)=>{
 res.send("Interview Scheduler Running")
@@ -392,12 +424,18 @@ db.query(
     console.log("📧 Manager:", managerEmail)
 
     // ✅ CREATE GOOGLE CALENDAR EVENT
+    const isoDate = convertToISODate(date)
+    const isoTime = convertTo24Hour(time)
+
+    console.log("🔥 BEFORE:", date, time)
+    console.log("✅ AFTER:", isoDate, isoTime)
+
     await createCalendarEvent({
-      date,
-      time,
-      candidateEmail,
-      managerEmail
-    })
+     date: isoDate,
+     time: isoTime,
+     candidateEmail,
+     managerEmail
+})
 
     // ✅ WHATSAPP TO CANDIDATE
     await sendMessage(candidatePhone,
